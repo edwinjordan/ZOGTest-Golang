1f7a749aa856e8bb7043a1c7ab07243b10bb50b8
edwinjordan <edwinlaksono12@gmail.com>
2025-08-30 16:07:03 +0700

    add schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+-- Enable UUID generator
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Table: topik
+CREATE TABLE IF NOT EXISTS topik (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    slug TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ NOT NULL 
+);
+
+-- Table: news
+CREATE TABLE IF NOT EXISTS news (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title TEXT NOT NULL,
+    slug TEXT NOT NULL,
+    content TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'draft',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    deleted_at TIMESTAMPTZ NOT NULL 
+);
+
+CREATE TABLE news_topic (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    topik_id TEXT NOT NULL,
+	news_id TEXT NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+	deleted_at TIMESTAMPTZ DEFAULT NOW()
+);
